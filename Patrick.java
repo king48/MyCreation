3812bf7167e20b07743d100bc43236d4e8b8a40a
Juan-Diego <juandiego085@gmail.com>
2018-03-26 18:51:59 -0400

    This is the first commit for this man

---
+++ Patrick.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import hsa.Console;
+
+
+public class Patrick
+{
+    private Console boo;
+
+    public void drawPatrick ()
+    {
+	// boo.setColor ();
+	//boo.fill
+    } 
+
+
+    public Patrick (Console con)
+    {
+	boo = con;
+    }
+}
