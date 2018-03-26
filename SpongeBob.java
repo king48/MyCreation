80a98780d4928a907aea55962a9160dceed6156f
Juan-Diego <juandiego085@gmail.com>
2018-03-26 18:52:54 -0400

    This is the first commit for this dude

---
+++ SpongeBob.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import hsa.Console;
+
+public class SpongeBob
+{
+    public drawingSponge ()
+    {
+    }
+
+
+    public movingSponge1 ()
+    {
+    }
+
+
+    public movingSponge2 ()
+    {
+    }
+
+
+
+
+    public spongeBob ()
+    {
+	bannana = con;
+
+    }
+}
