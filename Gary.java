d8d8dcff838b1292f1844c2657486b53f211c0eb
Juan-Diego <juandiego085@gmail.com>
2018-03-26 18:45:56 -0400

    This is gary first commit

---
+++ Gary.java
@@ -0,0 +1,17 @@
+import hsa.Console;
+import java.awt.*;
+
+public class Gary
+{
+    private Console fou;
+    
+    public drawGary ()
+    {
+    }
+
+
+    public Gary (Console con)
+    {
+	fou = con;
+    }
+}
