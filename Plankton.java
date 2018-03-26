741b37d5d65a4a372af590bc38714ceaa702c0c6
Juan-Diego <juandiego085@gmail.com>
2018-03-26 18:52:21 -0400

    This is the first commit for this dude

---
+++ Plankton.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import hsa.Console;
+
+public class Plankton
+{
+    private Console dude;
+    public drawPlankton ()
+    {
+    }
+    public Plankton (Console con)
+    {
+	dude = con;
+    }
+}
