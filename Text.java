5a5cbf66da523481f122ada94a7d9b5c4c944952
Juan-Diego <juandiego085@gmail.com>
2018-03-26 18:53:21 -0400

    This is the first commit for this stupid text

---
+++ Text.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import hsa.Console;
+
+public class Text
+{
+    private Console texto;
+    public void text ()
+    {
+    }
+
+
+    public Text (Console con)
+    {
+	texto = con;
+    }
+}
