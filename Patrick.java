50555170bda467425fbfe7cdf95d7958c9111374
Juan-Diego <juandiego085@gmail.com>
2018-03-26 19:26:14 -0400

    made patrick, and his eyes

--- Patrick.java
+++ Patrick.java
@@ -8,9 +8,14 @@ public class Patrick
 
     public void drawPatrick ()
     {
-	// boo.setColor ();
-	//boo.fill
-    } 
+	boo.setColor (new Color (245, 156, 96)); //Setting the pink body color
+	boo.fillStar (0, 295, 120, 140); // Drawing Patricks body
+
+	boo.setColor (Color.black); // Setting back eye color
+	boo.fillOval (54, 320, 5, 5); // Makes the two eyes of Patrick
+	boo.fillOval (62, 320, 5, 5); // Makes the two eyes of Patrick
+	boo.setColor ();
+    }
 
 
     public Patrick (Console con)
