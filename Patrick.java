66f4f7edfdda9759c0ad443d12ad52fcb95b40ce
Juan-Diego <juandiego085@gmail.com>
2018-03-26 21:31:12 -0400

    just finished him, he looks good

--- Patrick.java
+++ Patrick.java
@@ -14,14 +14,13 @@ public class Patrick
 	boo.setColor (Color.black); // Setting back eye color
 	boo.fillOval (54, 320, 5, 5); // Makes the two eyes of Patrick
 	boo.fillOval (62, 320, 5, 5); // Makes the two eyes of Patrick
+	
 	boo.drawArc (53, 330, 14, 10, 10, -180); // adds smile of Patrick
 
 	boo.setColor (new Color (250, 85, 255)); //Setting blue-ish pants color
 	boo.fillRect (30, 378, 60, 25); // top big pants
 	boo.fillRect (30, 396, 25, 20); //left leg
 	boo.fillRect (65, 396, 25, 20); //righ leg
-
-
 
     }
 
