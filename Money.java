eeae41132727dfb44a0aefd2b5a4bc65cb4b171c
Juan-Diego <juandiego085@gmail.com>
2018-03-27 18:19:03 -0400

    fianally is on screen but only one is on screen at a time :/

---
+++ Money.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import hsa.Console;
+import java.lang.*;
+
+public class Money extends Thread
+{
+    private Console boi;
+    Color color, a;
+    int speed, s;
+    public void drawingMoney ()
+    {
+	Color waterBlue = new Color (32, 180, 235);
+	for (int y = 0 ; y <= 200 ; y++)
+	{
+
+	    boi.setColor (color);
+	    boi.fillRect (100, 100 + y, 10, 10);
+	    
+	    boi.setColor (waterBlue);
+	    boi.fillRect (100, 100 + y, 10, 10);
+
+	    try
+	    {
+		sleep (s);
+	    }
+	    catch (Exception e)
+	    {
+	    }
+	}
+    }
+
+
+    public Money (Console con)
+    {
+	boi = con;
+	color = new Color (149, 149, 149);
+    }
+
+
+    public Money (Console con, Color color)
+    {
+	boi = con;
+	color = a;
+    }
+
+
+    public Money (Console con, Color color, int speed)
+    {
+	boi = con;
+	color = a;
+	speed = s;
+    }
+
+
+    public void run ()
+    {
+
+	drawingMoney ();
+    }
+}
+
+
