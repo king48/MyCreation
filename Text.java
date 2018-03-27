a337d7e62745de721cfe3179c6394cf46d9064bc
Juan-Diego <juandiego085@gmail.com>
2018-03-27 00:35:25 -0400

    we wored on overloading methods, in specific patrick

--- Text.java
+++ Text.java
@@ -4,8 +4,11 @@ import hsa.Console;
 public class Text
 {
     private Console texto;
-    public void text ()
+    public void textPatrick1 (int x, int y)
     {
+	texto.setColor (Color.black);
+	texto.setFont (new Font ("Arial", 1, 13));
+	texto.drawString ("Hey Sopnge Bob ", x, y);
     }
 
 
