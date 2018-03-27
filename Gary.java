2a776e777dfc86a717b4aab84f9e89987d193f38
Juan-Diego <juandiego085@gmail.com>
2018-03-27 00:52:17 -0400

    This just made the oval

--- Gary.java
+++ Gary.java
@@ -4,14 +4,18 @@ import java.awt.*;
 public class Gary
 {
     private Console fou;
-    
-    public drawGary ()
+
+    public void drawGary ()
     {
+	fou.setColor (new Color (128, 229, 65));
+	fou.fillOval (100, 100, 100, 20);
+
     }
 
 
     public Gary (Console con)
     {
 	fou = con;
+	drawGary ();
     }
 }
