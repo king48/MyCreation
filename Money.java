e55d0c2d89e5007ccce8e5c32909643f03a693e3
Castano <JuanDiego.Castano@student.tdsb.on.ca>
2018-03-27 14:00:54 -0400

    just made big changes to

--- Money.java
+++ Money.java
@@ -1,20 +1,23 @@
 import java.awt.*;
 import hsa.Console;
 
-public class Money
+public class Money extends Thread
 {
     private Console boi;
-
-    public void drawingMoney (int speed, int color1, int color2, int color3, int distance)
+    Color color;
+    int speed;
+    Color a;
+    int s;
+    public void drawingMoney ()
     {
-	for (int y = 0 ; y <= distance ; y++)
+	for (int y = 0 ; y <= 100 ; y++)
 	{
-	    boi.setColor (new Color (color1, color2, color3));
+	    boi.setColor (color);
 	    boi.fillRect (10, 100 + y, 10, 10);
 
 	    try
 	    {
-		Thread.sleep (speed);
+		Thread.sleep (s);
 	    }
 	    catch (Exception e)
 	    {
@@ -23,30 +26,30 @@ public class Money
     }
 
 
-    public void money1 ()
+    public void money1 (Console con)
     {
-	drawingMoney (100, 31, 233, 211, 350);
+	con = boi;
     }
 
 
-    public void money2()
+    public void money1 (Console con, Color color)
     {
+	con = boi;
+	color = a;
     }
 
 
-    public void money3()
+    public void money1 (Console con, Color color, int speed)
     {
+	con = boi;
+	color = a;
+	speed = s;
     }
 
 
     public Money (Console con)
     {
 	boi = con;
-	money1 ();
-	money2 ();
-	money3 ();
-
-
     }
 }
 
