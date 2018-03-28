f126f8b9d0f0d05aee3b4b7d954f6770903b32ca
Juan-Diego <juandiego085@gmail.com>
2018-03-28 16:30:19 -0400

    This is just adding the lang shit

--- Plankton.java
+++ Plankton.java
@@ -1,12 +1,17 @@
 import java.awt.*;
 import hsa.Console;
+import java.lang.*;
 
 public class Plankton
 {
     private Console dude;
+
     public drawPlankton ()
     {
+
     }
+
+
     public Plankton (Console con)
     {
 	dude = con;
