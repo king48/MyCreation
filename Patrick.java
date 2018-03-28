93ee3fdcf6116c2e0284a7a1344745ca2afd1eff
Khrislea Marquez <juandiego085@gmail.com>
2018-03-28 13:29:17 -0400

    We made the erase on this mans legs

--- Patrick.java
+++ Patrick.java
@@ -18,7 +18,21 @@ public class Patrick extends Thread
 	    boo.setColor (new Color (250, 85, 255)); //Setting blue-ish pants color
 	    boo.fillRect (30 + x, 378, 60, 25); // top big pants
 	    boo.fillRect (30 + x, 396, 25, 20); //left leg
-	    boo.fillRect (65 + x, 396, 25, 20); //righ leg
+	    boo.fillRect (65 + x, 396, 25, 20); //right leg
+
+	    boo.setColor (new Color (236, 220, 148));
+	    boo.drawLine (22 + x, 434, 30 + x, 410);
+	    boo.drawLine (21 + x, 434, 29 + x, 410);
+	    boo.drawLine (29 + x, 410, 29 + x, 400);
+	    boo.drawLine (28 + x, 410, 28 + x, 400);
+	    boo.drawLine (96 + x, 434, 75 + x, 415);
+	    boo.drawLine (95 + x, 434, 74 + x, 415);
+
+	    boo.drawLine (64 + x, 415, 64 + x, 404);
+	    boo.drawLine (65 + x, 415, 65 + x, 404);
+
+
+
 	    // still have to erase
 
 
