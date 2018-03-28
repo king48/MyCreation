a0b3be1bb0227ad093587086ed5e22e560a70060
Castano <JuanDiego.Castano@student.tdsb.on.ca>
2018-03-28 15:06:14 -0400

    IDEK wha happened

--- SpongeBob.java
+++ SpongeBob.java
@@ -10,7 +10,6 @@ public class SpongeBob
 	bannana.setColor (Color.black);
 	bannana.drawLine (70, 350, 70, 400); // drawling legs
 	bannana.drawLine (110, 350, 110, 400);
-
 	bannana.fillRect (70, 392, 18, 9);  //drawing feet
 	bannana.fillRect (110, 392, 18, 9);
 
