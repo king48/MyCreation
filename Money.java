c5a543d8e79fb9dbcf80bef40ec506ab6be6f940
Juan-Diego <juandiego085@gmail.com>
2018-03-28 16:27:22 -0400

    Overloaded works

--- Money.java
+++ Money.java
@@ -6,22 +6,21 @@ public class Money extends Thread
 {
     private Console boi;
     Color color, color2, color3, e, b, a;
-    int speed;
+    int speed = 20;
 
     public void drawingMoney ()
     {
 	Color waterBlue = new Color (32, 180, 235);
-	for (int y = 0 ; y <= 250 ; y++)
+	for (int y = 0 ; y <= 100 ; y++)
 	{
 	    boi.setColor (waterBlue);
-	    boi.fillRect (100, 59 + y, 20, 20);
+	    boi.fillRect (200, 59 + y, 20, 20);
 	    boi.setColor (color);
-	    boi.fillRect (100, 60 + y, 20, 20);
+	    boi.fillRect (200, 60 + y, 20, 20);
 	    boi.setColor (color2);
-	    boi.fillRect (100, 80 + y, 20, 20);
+	    boi.fillRect (200, 80 + y, 20, 20);
 	    boi.setColor (color3);
-	    boi.fillRect (100, 100 + y, 20, 20);
-
+	    boi.fillRect (200, 100 + y, 20, 20);
 
 	    try
 	    {
@@ -37,10 +36,9 @@ public class Money extends Thread
     public Money (Console con)
     {
 	boi = con;
-	color = new Color (51, 0, 25); // dark red
+	color = new Color (51, 204, 25); // dark red
 	color2 = new Color (255, 0, 0); // bright red
 	color3 = new Color (255, 204, 204); // light light red
-	int speed = 100;
 
     }
 
@@ -51,7 +49,7 @@ public class Money extends Thread
 	color = a;
 	color2 = b;
 	color3 = e;
-	int speed = 100;
+	speed = 50; 
     }
 
 
@@ -68,6 +66,7 @@ public class Money extends Thread
     public void start ()
     {
 	drawingMoney ();
+
     }
 }
 
