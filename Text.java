567524981a4364e8588ec035ba62b0df7659e839
Juan-Diego <juandiego085@gmail.com>
2018-03-29 04:12:37 -0400

    Text -- added quite a few methods for talking

--- Text.java
+++ Text.java
@@ -4,11 +4,34 @@ import hsa.Console;
 public class Text
 {
     private Console texto;
+    public void tryCatch ()
+    {
+	try
+	{
+	    Thread.sleep (100);
+	}
+	catch (Exception e)
+	{
+	}
+    }
+
+
     public void textPatrick1 (int x, int y)
     {
 	texto.setColor (Color.black);
 	texto.setFont (new Font ("Arial", 1, 13));
 	texto.drawString ("Hey SB ", x, y);
+	tryCatch ();
+    }
+
+
+    public void textPatrick2 (int x, int y)
+    {
+	texto.setColor (Color.black);
+	texto.setFont (new Font ("Arial", 1, 13));
+	texto.drawString ("Hey SB ", x, y);
+
+	tryCatch ();
     }
 
 
@@ -20,18 +43,24 @@ public class Text
     }
 
 
-    public void textPatrick2 (int x, int y)
-    {
-	//texto.setColor ();
-    }
-
-
     public void krabMoney (int x, int y)
     {
 	texto.setColor (Color.black);
 	texto.setFont (new Font ("Arial", 1, 13));
 	texto.drawString ("HA LOOK AT ALL THIS MONEY", x, y);
 	texto.drawString ("I made it selling my burgers", x, y - 20);
+    }
+
+
+    public void spongeBobTalk (int x, int y)
+    {
+
+    }
+
+
+    public void spongeBobTalk2 (int x, int y)
+    {
+
     }
 
 
