7aa7495de4ce8f5935ddc152f93864bfbbff8e28
Juan-Diego <juandiego085@gmail.com>
2018-03-29 03:20:35 -0400

    SpongeBob made major changes, moved him in another direction, it works well though

--- SpongeBob.java
+++ SpongeBob.java
@@ -4,14 +4,15 @@ import java.lang.*;
 public class SpongeBob extends Thread
 {
     private Console bannana;
-    public void drawingSponge (int d)
+    int distance = 150;
+    public void drawingSponge (int speed)
     {
-	for (int i = 0 ; i <= d ; i++)
+	for (int i = 0 ; i <= distance ; i++)
 	{
 	    bannana.setColor (new Color (236, 220, 148));
 	    bannana.fillRect (641 - i, 300, 80, 100); // Drawing SpongeBobs body
 	    bannana.drawLine (661 - i, 400, 661 - i, 450); // Drawing SpongeBobs legs
-	    bannana.drawLine (701 - i, 400, 701- i, 450);
+	    bannana.drawLine (701 - i, 400, 701 - i, 450);
 	    bannana.fillOval (659 - i, 320, 13, 13); // Drawing SpongeBobs eyes
 	    bannana.fillOval (691 - i, 320, 13, 13);
 	    bannana.drawArc (665 - i, 340, 30, 20, 0, -180);
@@ -31,7 +32,7 @@ public class SpongeBob extends Thread
 
 	    try
 	    {
-		Thread.sleep (50);
+		Thread.sleep (speed);
 	    }
 	    catch (Exception e)
 	    {
@@ -42,8 +43,93 @@ public class SpongeBob extends Thread
 
     public void movingSponge1 ()
     {
-	drawingSponge (150);
+	Text t = new Text (bannana);
+
+	drawingSponge (10);
+
+
+	try
+	{
+	    Thread.sleep (100);
+	}
+	catch (Exception e)
+	{
+	}
+
+	t.spongeBobTalk (150, 100);
     }
+
+
+    public void drawingSponge2 (int d2, int speed2)
+    {
+	for (int i = 0 ; i <= d2 ; i++)
+	{
+	// Erases SpongeBob 
+	    bannana.setColor (new Color (236, 220, 148));
+	    bannana.fillRect (640 - distance, 301 - i, 80, 100); // Drawing SpongeBobs body
+	    bannana.drawLine (660 - distance, 401 - i, 660 - distance, 450 - i); // Drawing SpongeBobs legs
+	    bannana.drawLine (700 - distance, 401 - i, 700 - distance, 450 - i);
+	    bannana.fillOval (658 - distance, 321 - i, 13, 13); // Drawing SpongeBobs eyes
+	    bannana.fillOval (690 - distance, 321 - i, 13, 13); 
+	    bannana.drawArc (664 - distance, 341 - i, 30, 20, 0, -180);
+	    bannana.fillRect (642 - distance, 443 - i, 19, 9); //Drawing SpongeBobs feet
+	    bannana.fillRect (682 - distance, 443 - i, 19, 9);
+	    //----------------------------------------------------//
+	 // Draws SopngeBob 
+	    bannana.setColor (new Color (255, 255, 0));
+	    bannana.fillRect (640 - distance, 300 - i, 80, 100); // Drawing SpongeBobs body
+	    bannana.setColor (Color.black);
+	    bannana.drawLine (660 - distance, 400 - i, 660 - distance, 450 - i); // Drawing SpongeBobs legs
+	    bannana.drawLine (700 - distance, 400 - i, 700 - distance, 450 - i);
+	    bannana.fillOval (658 - distance, 320 - i, 13, 13); // Drawing SpongeBobs eyes
+	    bannana.fillOval (690 - distance, 320 - i, 13, 13);
+	    bannana.drawArc (664 - distance, 340 - i, 30, 20, 0, -180);
+	    bannana.fillRect (642 - distance, 442 - i, 18, 9); //Drawing SpongeBobs feet
+	    bannana.fillRect (682 - distance, 442 - i, 18, 9);
+	    try
+	    {
+		Thread.sleep (speed2);
+	    }
+	    catch (Exception e)
+	    {
+	    }
+	}
+    }
+
+
+    public void movingSponge2 ()
+    {
+	Text t = new Text (bannana); // allwos me to call on the Text class
+	drawingSponge2 (10, 10); // Controls distance and speed when sponge bob is going up
+
+	try
+	{
+	    Thread.sleep (100); // gives us a ___ delay
+	}
+	catch (Exception e)
+	{
+	}
+
+	t.spongeBobTalk2 (20, 100); // Allows us to position the text where we see fit after a cirtain period of time
+    }
+
+
+    public void run ()
+    {
+	movingSponge1 ();
+
+	try
+	{
+	    Thread.sleep (1000);
+	}
+	catch (Exception e)
+	{
+	}
+
+	movingSponge2 ();
+    }
+
+
 
 
     public SpongeBob (Console con)
@@ -51,10 +137,5 @@ public class SpongeBob extends Thread
     {
 	bannana = con;
     }
-
-
-    public void run ()
-    {
-	movingSponge1 ();
-    }
 }
+
