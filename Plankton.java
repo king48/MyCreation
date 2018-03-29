5184e91e013f3276a0cda8f84638220a5cb4efa0
Juan-Diego <juandiego085@gmail.com>
2018-03-29 02:04:35 -0400

    Plankton -- changes were made to his movement, attempt to overload falied :/

--- Plankton.java
+++ Plankton.java
@@ -10,29 +10,29 @@ public class Plankton implements Runnable
 	for (int x = 0 ; x <= x2 ; x++)
 	{
 	    dude.setColor (new Color (236, 220, 148));
-	    dude.fillOval (9 + x, 402, 15, 50); // draws Planktons body
+	    dude.fillOval (9 + x, 402, 15, 50);  // draws Planktons body
 	    dude.setColor (new Color (236, 220, 148));
-	    dude.fillOval (12 + x, 412, 4, 4); // draws Planktons eyes
+	    dude.fillOval (12 + x, 412, 4, 4);  // draws Planktons eyes
 	    dude.fillOval (18 + x, 412, 4, 4);
 	    // dude.drawLine ( , , , ); // draws all of Planktons limbs
 	    // dude.drawLine ( , , , );
 	    // dude.drawLine ( , , , );
 	    // dude.drawLine ( , , , );
 	    dude.setColor (new Color (236, 220, 148));
-	    dude.fillRect (9 + x, 403, 15, 7); //draws Planktons hat
+	    dude.fillRect (9 + x, 403, 15, 7);  //draws Planktons hat
 	    dude.fillRect (13 + x, 394, 7, 9);
 	    //-------------------------------------------------------------//
 	    dude.setColor (new Color (0, 101, 50));
 	    dude.fillOval (10 + x, 402, 15, 50); // draws Planktons body
 	    dude.setColor (Color.red);
-	    dude.fillOval (13 + x, 412, 4, 4); // draws Planktons eyes
+	    dude.fillOval (13 + x, 412, 4, 4);  // draws Planktons eyes
 	    dude.fillOval (19 + x, 412, 4, 4);
 	    // dude.drawLine ( , , , ); // draws all of Planktons limbs
 	    // dude.drawLine ( , , , );
 	    // dude.drawLine ( , , , );
 	    // dude.drawLine ( , , , );
 	    dude.setColor (new Color (153, 204, 255));
-	    dude.fillRect (10 + x, 403, 15, 7); //draws Planktons hat
+	    dude.fillRect (10 + x, 403, 15, 7);  //draws Planktons hat
 	    dude.fillRect (14 + x, 394, 7, 9);
 
 	    try
@@ -49,9 +49,12 @@ public class Plankton implements Runnable
 
     public void planktonMovement1 ()
     {
-	drawPlankton (100, 100);
+	drawPlankton (10, 10);
 
     }
+
+
+
 
 
     public Plankton (Console con)
