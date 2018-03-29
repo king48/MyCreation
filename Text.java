6774dfbfd07f109c89ab30916baff7b800ec5e07
Juan-Diego <juandiego085@gmail.com>
2018-03-29 03:19:31 -0400

    Text -- Added a lot of diaglauge and delays, annndd erasing

--- Text.java
+++ Text.java
@@ -26,6 +26,15 @@ public class Text
     }
 
 
+    public void krabMoney (int x, int y)
+    {
+	texto.setColor (Color.black);
+	texto.setFont (new Font ("Arial", 1, 13));
+	texto.drawString ("HA LOOK AT ALL THIS MONEY", x, y);
+	texto.drawString ("I made it selling my burgers", x, y - 20);
+    }
+
+
     public Text (Console con)
     {
 	texto = con;
