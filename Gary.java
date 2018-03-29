2d2d466665faf6e31120bf79dd648018315f9378
Juan-Diego <juandiego085@gmail.com>
2018-03-29 00:48:06 -0400

    i fixed the issue

--- Gary.java
+++ Gary.java
@@ -5,50 +5,56 @@ import java.lang.*;
 public class Gary extends Thread
 {
     private Console fou;
-    int speed = 100;
-    public void drawGary ()
+    public void drawGary (int x2, int speed)
     {
-	for (int i = 0 ; i <= 200 ; i++)
+	for (int i = 0 ; i <= x2 ; i++)
 	{
 	    // erase
 	    fou.setColor (new Color (32, 180, 235));
-	    fou.fillOval (641 - i, 100, 100, 20);
-	    fou.fillRect (646 - i, 80, 5, 35);
-	    fou.fillRect (656 - i, 80, 5, 35); // Fraws the supports to garys eyes
-	    fou.fillArc (671 - i, 60, 60, 100, 0, 180); // Draws Garys shell
-	    fou.fillOval (644 - i, 75, 9, 9); // Draws garys eyes
-	    fou.fillOval (654 - i, 75, 9, 9);
-	    fou.fillOval (647 - i, 78, 3, 3); //Draws garys puples
-	    fou.fillOval (657 - i, 78, 3, 3);
+	    fou.fillOval (641 - i, 400, 100, 20);
+	    fou.fillRect (646 - i, 380, 5, 35);
+	    fou.fillRect (656 - i, 380, 5, 35); // Fraws the supports to garys eyes
+	    fou.fillArc (671 - i, 360, 60, 100, 0, 180); // Draws Garys shell
+	    fou.fillOval (644 - i, 375, 9, 9); // Draws garys eyes
+	    fou.fillOval (654 - i, 375, 9, 9);
+	    fou.fillOval (647 - i, 378, 3, 3); //Draws garys puples
+	    fou.fillOval (657 - i, 378, 3, 3);
 	    // fou.drawLine (10, 10, 120, 120);
-	    fou.fillMapleLeaf (670 - i, 80, 20, 20); // Draws the maple leaf of Garys shell
+	    fou.fillMapleLeaf (670 - i, 380, 20, 20); // Draws the maple leaf of Garys shell
 
 	    //---------------------------------------------------------------------//
 
 	    // draw
 	    fou.setColor (new Color (128, 229, 65));
-	    fou.fillOval (640 - i, 100, 100, 20);
-	    fou.fillRect (645 - i, 80, 5, 35);
-	    fou.fillRect (655 - i, 80, 5, 35); // Fraws the supports to garys eyes
+	    fou.fillOval (640 - i, 400, 100, 20);
+	    fou.fillRect (645 - i, 380, 5, 35);
+	    fou.fillRect (655 - i, 380, 5, 35); // Fraws the supports to garys eyes
 	    fou.setColor (new Color (255, 51, 255));
-	    fou.fillArc (670 - i, 60, 60, 100, 0, 180); // Draws Garys shell
+	    fou.fillArc (670 - i, 360, 60, 100, 0, 180); // Draws Garys shell
 	    fou.setColor (Color.black); // Sets the eye color to back
-	    fou.fillOval (643 - i, 75, 9, 9); // Draws garys eyes
-	    fou.fillOval (653 - i, 75, 9, 9);
+	    fou.fillOval (643 - i, 375, 9, 9); // Draws garys eyes
+	    fou.fillOval (653 - i, 375, 9, 9);
 	    fou.setColor (Color.white); // Sets the puple color to white
-	    fou.fillOval (646 - i, 78, 3, 3); //Draws garys puples
-	    fou.fillOval (656 - i, 78, 3, 3);
+	    fou.fillOval (646 - i, 378, 3, 3); //Draws garys puples
+	    fou.fillOval (656 - i, 378, 3, 3);
 	    // fou.drawLine (10, 10, 120, 120);
 	    fou.setColor (new Color (165, 128, 250)); // Sets the light purple color
-	    fou.fillMapleLeaf (689 - i, 80, 20, 20); // Draws the maple leaf of Garys shell
+	    fou.fillMapleLeaf (689 - i, 380, 20, 20); // Draws the maple leaf of Garys shell
+
+	    try
+	    {
+		Thread.sleep (speed);
+	    }
+	    catch (Exception e)
+	    {
+	    }
 	}
-	try
-	{
-	    Thread.sleep (speed);
-	}
-	catch (Exception e)
-	{
-	}
+    }
+
+
+    public void garyMovement1 ()
+    {
+	drawGary (300, 100);
     }
 
 
@@ -58,8 +64,8 @@ public class Gary extends Thread
     }
 
 
-    public void start ()
+    public void run ()
     {
-	drawGary ();
+	garyMovement1 ();
     }
 }
